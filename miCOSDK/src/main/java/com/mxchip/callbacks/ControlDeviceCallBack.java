8169587fef2ceeceae5afafaae9a6aa2d07b6013
bringmehome <bringmehome@vip.qq.com>
2016-04-14 15:38:29 +0800

    remove all

--- miCOSDK/src/main/java/com/mxchip/callbacks/ControlDeviceCallBack.java
+++
@@ -1,7 +0,0 @@
-package com.mxchip.callbacks;
-
-public interface ControlDeviceCallBack {
-	public void onSuccess(String message);
-	public void onFailure(int code, String message);
-	public void onDeviceStatusReceived(String msgType, String messages);
-}
--- miCOSDK/src/main/java/com/mxchip/mqttservice2/MqttServiceListener.java
+++
@@ -1,5 +0,0 @@
-package com.mxchip.mqttservice2;
-
-public interface MqttServiceListener {
-	public void onMqttReceiver(String msgType, String messages);
-}
